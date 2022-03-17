ce3e16ea1ade3cc46c87933047fc06f22a22e661
Jan Horacek <me@apophis.cz>
2022-03-17 16:56:17 +0100

    Add io.

---
+++ fw/src/io.c
@@ -0,0 +1,78 @@
+#include <avr/io.h>
+#include "io.h"
+
+void set_output(uint8_t pin, bool state) {
+	if (pin >= IO_PINB0 && pin <= IO_PINB7) {
+		if (state)
+			PORTB |= (1 << (pin-IO_PINB0));
+		else
+			PORTB &= ~(1 << (pin-IO_PINB0));
+	} else if (pin >= IO_PINC0 && pin <= IO_PINC6) {
+		if (state)
+			PORTC |= (1 << (pin-IO_PINC0));
+		else
+			PORTC &= ~(1 << (pin-IO_PINC0));
+	} else if (pin >= IO_PIND0 && pin <= IO_PIND7) {
+		if (state)
+			PORTD |= (1 << (pin-IO_PIND0));
+		else
+			PORTD &= ~(1 << (pin-IO_PIND0));
+	}
+}
+
+bool get_input(uint8_t pin) {
+	if (pin >= IO_PINB0 && pin <= IO_PINB7)
+		return (PINB >> (pin-IO_PINB0)) & 1;
+	if (pin >= IO_PINC0 && pin <= IO_PINC6)
+		return (PINC >> (pin-IO_PINC0)) & 1;
+	if (pin >= IO_PIND0 && pin <= IO_PIND7)
+		return (PIND >> (pin-IO_PIND0)) & 1;
+
+	return false;
+}
+
+void pin_mode(uint8_t pin, uint8_t mode) {
+	if (mode == OUTPUT) {
+		if (pin >= IO_PINB0 && pin <= IO_PINB7) {
+			DDRB |= (1 << (pin-IO_PINB0));
+		} else if (pin >= IO_PINC0 && pin <= IO_PINC6) {
+			DDRC |= (1 << (pin-IO_PINC0));
+		} else if (pin >= IO_PIND0 && pin <= IO_PIND7) {
+			DDRD |= (1 << (pin-IO_PIND0));
+		}
+	} else {
+		set_output(pin, mode == INPUT_PULLUP);
+
+		if (pin >= IO_PINB0 && pin <= IO_PINB7) {
+			DDRB &= ~(1 << (pin-IO_PINB0));
+		} else if (pin >= IO_PINC0 && pin <= IO_PINC6) {
+			DDRC &= ~(1 << (pin-IO_PINC0));
+		} else if (pin >= IO_PIND0 && pin <= IO_PIND7) {
+			DDRD &= ~(1 << (pin-IO_PIND0));
+		}
+	}
+}
+
+void io_init() {
+	PORTB = 0; // reset outputs to 0
+	PORTC = 0; // reset outputs to 0
+	PORTD = 0; // reset outputs to 0
+
+	pin_mode(PIN_IN_PLUS, INPUT);
+	pin_mode(PIN_IN_MINUS, INPUT);
+	pin_mode(PIN_OUT_PLUS, OUTPUT);
+	pin_mode(PIN_OUT_MINUS, OUTPUT);
+
+	pin_mode(PIN_BTN_PLUS_IN, INPUT);
+	pin_mode(PIN_BTN_PLUS_OUT, OUTPUT);
+	pin_mode(PIN_BTN_MINUS_IN, INPUT);
+	pin_mode(PIN_BTN_MINUS_OUT, OUTPUT);
+
+	pin_mode(PIN_SERVO_POWER_EN, OUTPUT);
+	pin_mode(PIN_BTN_IN, INPUT);
+	pin_mode(PIN_RELAY_CONTROL, OUTPUT);
+
+	pin_mode(PIN_LED_RED, OUTPUT);
+	pin_mode(PIN_LED_YELLOW, OUTPUT);
+	pin_mode(PIN_LED_GREEN, OUTPUT);
+}
---
+++ fw/src/io.h
@@ -0,0 +1,65 @@
+#ifndef _IO_H_
+#define _IO_H_
+
+/* General digital IO functions, simillar to Arduino. */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define OUTPUT 0
+#define INPUT 1
+#define INPUT_PULLUP 2
+
+#define IO_PINB0 0
+#define IO_PINB1 1
+#define IO_PINB2 2
+#define IO_PINB3 3
+#define IO_PINB4 4
+#define IO_PINB5 5
+#define IO_PINB6 6
+#define IO_PINB7 7
+
+#define IO_PINC0 8
+#define IO_PINC1 9
+#define IO_PINC2 10
+#define IO_PINC3 11
+#define IO_PINC4 12
+#define IO_PINC5 13
+#define IO_PINC6 14
+
+#define IO_PIND0 16
+#define IO_PIND1 17
+#define IO_PIND2 18
+#define IO_PIND3 19
+#define IO_PIND4 20
+#define IO_PIND5 21
+#define IO_PIND6 22
+#define IO_PIND7 23
+
+
+#define PIN_IN_PLUS IO_PINC3
+#define PIN_IN_MINUS IO_PINC2
+#define PIN_OUT_PLUS IO_PINB2
+#define PIN_OUT_MINUS IO_PIND2
+
+#define PIN_BTN_PLUS_IN IO_PINC5
+#define PIN_BTN_PLUS_OUT IO_PIND3
+#define PIN_BTN_MINUS_IN IO_PINC4
+#define PIN_BTN_MINUS_OUT IO_PINB6
+
+#define PIN_SERVO_POWER_EN IO_PINB0
+#define PIN_SERVO_PWM IO_PINB1
+#define PIN_SERVO_VCC_IN IO_PINC0
+#define PIN_BTN_IN IO_PINC1
+#define PIN_RELAY_CONTROL IO_PIND4
+
+#define PIN_LED_RED IO_PIND7
+#define PIN_LED_YELLOW IO_PIND6
+#define PIN_LED_GREEN IO_PIND5
+
+void set_output(uint8_t pin, bool state);
+bool get_input(uint8_t pin);
+void pin_mode(uint8_t pin, uint8_t mode);
+void io_init();
+
+#endif
