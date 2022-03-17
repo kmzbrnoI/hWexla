05e9c97e85dbc2a0cf9ed90f9c94b5b476c73b9a
Jan Horacek <me@apophis.cz>
2022-03-17 16:56:06 +0100

    Add timer2 @ 1kHz.

--- fw/src/main.c
+++ fw/src/main.c
@@ -8,6 +8,9 @@
 #include <avr/io.h>
 #include <avr/wdt.h>
 #include <avr/eeprom.h>
+#include <avr/boot.h>
+
+#include "io.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -20,6 +23,12 @@ int main() {
 	init();
 
 	while (true) {
+		// problem with PIN_IN_MINUS, PIN_BTN_PLUS_IN, PIN_BTN_MINUS_IN
+		//set_output(PIN_OUT_PLUS, !get_input(PIN_BTN_PLUS_IN));
+		set_output(PIN_LED_RED, !get_input(PIN_IN_MINUS));
+		set_output(PIN_LED_YELLOW, !get_input(PIN_IN_PLUS));
+		set_output(PIN_RELAY_CONTROL, !get_input(PIN_BTN_IN));
+
 		_delay_ms(1);
 		wdt_reset();
 	}
@@ -27,14 +36,33 @@ int main() {
 
 static inline void init() {
 	ACSR |= ACD;  // analog comparator disable
-	wdt_enable(WDTO_250MS);
+	io_init();
+
+	// Timer 2 @ 1 kHz (1 ms)
+	TCCR2 = (1 << WGM21) | (1 << CS21) | (1 << CS20); // CTC; prescaler 32×
+	OCR2 = 248; // 1 ms
+	TIMSK = (1 << OCIE2);
+
+	set_output(PIN_LED_RED, true);
+	set_output(PIN_LED_YELLOW, true);
+	set_output(PIN_LED_GREEN, true);
+	_delay_ms(250);
+	set_output(PIN_LED_RED, false);
+	set_output(PIN_LED_YELLOW, false);
+
+	set_output(PIN_LED_GREEN, true);
+	set_output(PIN_SERVO_POWER_EN, true);
 
 	sei(); // enable interrupts globally
+	wdt_enable(WDTO_250MS);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-ISR(TIMER0_COMPA_vect) {
+ISR(TIMER2_COMP_vect) {
+	set_output(PIN_OUT_PLUS, true);
+	_delay_us(10);
+	set_output(PIN_OUT_PLUS, false);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
