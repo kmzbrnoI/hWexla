554714add3625b33030725b310dcf4c48cd03f84
Jan Horacek <me@apophis.cz>
2022-11-13 12:49:01 +0100

    read.py: show terminal properly.

--- fw/src/read.py
+++ fw/src/read.py
@@ -4,13 +4,24 @@
 import serial
 import sys
 import datetime
+import select
 
 
 assert len(sys.argv) >= 2
 
 ser = serial.Serial(port=sys.argv[1], baudrate=38400)
 
+# Wait until beginning of browser (special terminal sequence 'move n lines above')
+byte = ser.read(1)
+while byte != b'\x1B':
+    byte = ser.read(1)
+
+ser.read(4) # read rest of the special sequence
+
 while True:
     line = ser.readline()
+    if line.startswith(b'\x1B'):
+        special, line = line[:5], line[5:]
+        print(special.decode('ascii'), end='')
     print(f'[{datetime.datetime.now().time()}]', end=' ')
-    print(line.decode('utf-8'), end='')
+    print(line.decode('ascii'), end='')
