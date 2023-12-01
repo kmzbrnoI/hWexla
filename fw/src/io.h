8fb7baec444052729dfbcf5b078a2465042c9750
Jan Horacek <me@apophis.cz>
2023-12-01 16:49:27 +0100

    Loosen up servo VCC checks (4.5/4.0 V).

--- fw/src/io.h
+++ fw/src/io.h
@@ -60,8 +60,8 @@
 
 #define MAG_MAX_VALUE 1023
 
-#define SERVO_VCC_MIN_WARN 481 // 4.7 V (= x/512 * 5.0)
-#define SERVO_VCC_MIN 430 // 4.2 V (= x/512 * 5.0)
+#define SERVO_VCC_MIN_WARN 461 // 4.5 V (= x/512 * 5.0)
+#define SERVO_VCC_MIN 410 // 4.0 V (= x/512 * 5.0)
 #define SERVO_VCC_MAX_WARN  532 // 5.2 V (= x/512 * 5.0)
 #define SERVO_VCC_MAX 563 // 5.5 V (= x/512 * 5.0)
 
